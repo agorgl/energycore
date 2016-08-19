202e93f13e050a75e3035a1ded4569122c10bfcd
TheArtist <agorglouk@gmail.com>
2016-08-20 00:47:18 +0300

    Added OpenGL debug callback

--- src/game.c
+++ src/game.c
@@ -1,7 +1,24 @@
 #include "game.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <glad/glad.h>
 #include "window.h"
 #include "input.h"
+
+static void gl_debug_proc(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* user_param)
+{
+    (void) source;
+    (void) id;
+    (void) severity;
+    (void) length;
+    (void) user_param;
+
+    if (type == GL_DEBUG_TYPE_ERROR) {
+        fprintf(stderr, "%s", message);
+        exit(1);
+    }
+}
 
 static void on_key(struct window* wnd, int key, int scancode, int action, int mods)
 {
@@ -26,6 +43,9 @@ void game_init(struct game_context* ctx)
     memset(&wnd_callbacks, 0, sizeof(struct window_callbacks));
     wnd_callbacks.key_cb = on_key;
     window_set_callbacks(ctx->wnd, &wnd_callbacks);
+
+    /* Setup OpenGL debug handler */
+    glDebugMessageCallback(gl_debug_proc, ctx);
 }
 
 void game_update(void* userdata, float dt)
