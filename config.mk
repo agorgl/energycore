2bade97e7634804d3c486ae24a763cf68eb6c004
TheArtist <agorglouk@gmail.com>
2016-08-19 23:52:17 +0300

    Added Linux support

--- config.mk
+++ config.mk
@@ -2,4 +2,6 @@ PRJTYPE = Executable
 LIBS = glfw glad macu
 ifeq ($(OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 winmm ole32 user32 shell32
+else
+	LIBS += GL GLU X11 Xrandr Xinerama Xcursor pthread dl
 endif
