0fa7364c01a9a700227c79146972a29c987dad9d
TheArtist <agorglouk@gmail.com>
2016-08-19 23:52:00 +0300

    Upgraded Glfw version

--- deps/Glfw/config.mk
+++ deps/Glfw/config.mk
@@ -4,7 +4,8 @@ SRC = \
 	src/init.c    \
 	src/input.c   \
 	src/monitor.c \
-	src/window.c
+	src/window.c  \
+	src/vulkan.c
 ifeq ($(OS), Windows_NT)
 	SRC += \
 		src/win32_init.c     \
@@ -12,8 +13,9 @@ ifeq ($(OS), Windows_NT)
 		src/win32_time.c     \
 		src/win32_tls.c      \
 		src/win32_window.c   \
-		src/winmm_joystick.c \
-		src/wgl_context.c
+		src/win32_joystick.c \
+		src/wgl_context.c    \
+		src/egl_context.c
 else
 	SRC += \
 		src/x11_init.c       \
@@ -23,19 +25,13 @@ else
 		src/linux_joystick.c \
 		src/posix_time.c     \
 		src/posix_tls.c      \
-		src/glx_context.c
+		src/glx_context.c    \
+		src/egl_context.c
 endif
 
-DEFINES = _GLFW_USE_OPENGL
+DEFINES =
 ifeq ($(OS), Windows_NT)
-	DEFINES += \
-		_GLFW_WIN32 \
-		_GLFW_WGL
+	DEFINES += _GLFW_WIN32
 else
-	DEFINES += \
-		_GLFW_X11 \
-		_GLFW_GLX \
-		_GLFW_HAS_GLXGETPROCADDRESS
+	DEFINES += _GLFW_X11
 endif
-
-ADDINCS = contrib/GL/include
