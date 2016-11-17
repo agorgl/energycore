514745b9a4cb3f9b12deb8700e864e37dd433e98
TheArtist <agorglouk@gmail.com>
2016-11-17 16:06:36 +0200

    Added AssetLoader external dependency

--- demo/config.mk
+++ demo/config.mk
@@ -1,9 +1,9 @@
 PRJTYPE = Executable
-LIBS = energycore gfxwnd glfw glad macu
+LIBS = energycore assetloader vorbis ogg freetype png jpeg tiff zlib gfxwnd glfw glad macu
 ifeq ($(TARGET_OS), Windows_NT)
 	LIBS += glu32 opengl32 gdi32 winmm ole32 user32 shell32
 else
 	LIBS += GL GLU X11 Xrandr Xinerama Xcursor pthread dl
 endif
-EXTDEPS = gfxwnd::0.0.0dev macu::0.0.1dev
+EXTDEPS = gfxwnd::0.0.0dev macu::0.0.1dev assetloader::dev
 MOREDEPS = ..
