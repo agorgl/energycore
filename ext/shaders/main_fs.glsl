82837ef92986bf70922a6237555bd5f38d4b29b1
TheArtist <agorglouk@gmail.com>
2016-11-21 14:47:18 +0200

    Implemented sample sphere probe rendering

--- ext/shaders/main_fs.glsl
+++ ext/shaders/main_fs.glsl
@@ -4,12 +4,20 @@ in vec3 Normal;
 in vec3 FragPos;
 out vec4 out_color;
 
+uniform samplerCube sky;
+uniform int render_mode;
 const vec3 lightPos = vec3(0.8, 1.0, 0.8);
 
 void main()
 {
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diffuse = max(dot(Normal, lightDir), 0.0);
-    vec3 result = vec3(1.0, 1.0, 1.0) * diffuse;
-    out_color = vec4(result, 1.0);
+    if (render_mode == 0) {
+        vec3 lightDir = normalize(lightPos - FragPos);
+        float diffuse = max(dot(Normal, lightDir), 0.0);
+        vec3 result = vec3(1.0, 1.0, 1.0) * diffuse;
+        out_color = vec4(result, 1.0);
+    } else if (render_mode == 1) {
+        vec3 normYInv = vec3(Normal.x, -Normal.y, Normal.z);
+        vec3 skycol = texture(sky, normYInv).xyz;
+        out_color = vec4(skycol, 1.0);
+    }
 };
--- src/renderer.c
+++ src/renderer.c
@@ -52,6 +52,54 @@ static void render_skybox(struct renderer_state* rs, mat4* view, mat4* proj, uns
     glDepthMask(GL_TRUE);
 }
 
+static void render_probe(struct renderer_state* rs, struct renderer_input* ri)
+{
+    /* Create sphere data */
+    struct sphere_gdata* vdat = uv_sphere_create(1.0f, 64, 64);
+    /* Create vao */
+    GLuint sph_vao, sph_vbo, sph_ebo;
+    glGenVertexArrays(1, &sph_vao);
+    glBindVertexArray(sph_vao);
+    /* Create vbo */
+    glGenBuffers(1, &sph_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, sph_vbo);
+    glBufferData(GL_ARRAY_BUFFER, vdat->num_verts * sizeof(struct sphere_vertice), vdat->vertices, GL_STATIC_DRAW);
+    /* Setup vertex inputs */
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(struct sphere_vertice), (GLvoid*)offsetof(struct sphere_vertice, position));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(struct sphere_vertice), (GLvoid*)offsetof(struct sphere_vertice, uvs));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(struct sphere_vertice), (GLvoid*)offsetof(struct sphere_vertice, normal));
+    /* Create ebo */
+    glGenBuffers(1, &sph_ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sph_ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, vdat->num_indices * sizeof(uint32_t), vdat->indices, GL_STATIC_DRAW);
+
+    /* Render setup */
+    glUseProgram(rs->shdr_main);
+    glUniform1i(glGetUniformLocation(rs->shdr_main, "sky"), 0);
+    glUniform1i(glGetUniformLocation(rs->shdr_main, "render_mode"), 1);
+    float scale = 0.2f;
+    mat4 model = mat4_mul_mat4(
+        mat4_scale(vec3_new(scale, scale, scale)),
+        mat4_translation(vec3_new(0.0f, 0.0f, 0.0f)));
+    glUniformMatrix4fv(glGetUniformLocation(rs->shdr_main, "model"), 1, GL_TRUE, (GLfloat*)&model);
+    /* Render */
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, ri->skybox);
+    glDrawElements(GL_TRIANGLES, vdat->num_indices, GL_UNSIGNED_INT, (void*)0);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+    glUseProgram(0);
+
+    /* Free */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    glDeleteBuffers(1, &sph_vbo);
+    glDeleteVertexArrays(1, &sph_vao);
+    uv_sphere_destroy(vdat);
+}
+
 void renderer_render(struct renderer_state* rs, struct renderer_input* ri, float view[16])
 {
     /* Clear default buffers */
@@ -65,6 +113,7 @@ void renderer_render(struct renderer_state* rs, struct renderer_input* ri, float
     glEnable(GL_DEPTH_TEST);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glUseProgram(rs->shdr_main);
+    glUniform1i(glGetUniformLocation(rs->shdr_main, "render_mode"), 0);
 
     /* Setup matrices */
     GLuint proj_mat_loc = glGetUniformLocation(rs->shdr_main, "proj");
@@ -87,6 +136,9 @@ void renderer_render(struct renderer_state* rs, struct renderer_input* ri, float
         glBindVertexArray(0);
     }
     glUseProgram(0);
+
+    /* Render sample probe */
+    render_probe(rs, ri);
 
     /* Render skybox last */
     render_skybox(rs, (mat4*)view, &proj, ri->skybox);
