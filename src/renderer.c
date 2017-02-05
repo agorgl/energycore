aa35e604063608b600f095710eb889d73bec57d2
TheArtist <agorglouk@gmail.com>
2017-02-05 20:26:04 +0200

    Disabled GI stuff by default

--- src/renderer.c
+++ src/renderer.c
@@ -216,6 +216,7 @@ static void render_scene(struct renderer_state* rs, struct renderer_input* ri, m
     render_sky(rs, ri, view->m, proj->m);
 }
 
+#ifdef WITH_GI
 /* Local callback data bundle */
 struct sh_gi_render_scene_userdata {
     struct renderer_state* rs;
@@ -246,6 +247,7 @@ static void sh_gi_prepare_gi_render(mat4* view, mat4* proj, void* userdata)
     struct sh_gi_render_scene_userdata* ud = userdata;
     bind_gbuffer_textures(ud->rs, ud->rs->sh_gi_rs->shdr, view, proj);
 }
+#endif
 
 /*-----------------------------------------------------------------
  * Public interface
@@ -255,13 +257,14 @@ void renderer_render(struct renderer_state* rs, struct renderer_input* ri, float
     /* Render main scene */
     render_scene(rs, ri, (mat4*)view, &rs->proj);
     /* Render GI */
+#ifdef WITH_GI
     struct sh_gi_render_scene_userdata ud;
     ud.rs = rs;
     ud.ri = ri;
     sh_gi_update(rs->sh_gi_rs, sh_gi_render_lcm_side, &ud);
     sh_gi_render(rs->sh_gi_rs, view, rs->proj.m, sh_gi_prepare_gi_render, &ud);
-    /* Visualize probes (SH mode) */
     sh_gi_vis_probes(rs->sh_gi_rs, view, rs->proj.m, 1);
+#endif
 }
 
 void renderer_resize(struct renderer_state* rs, unsigned int width, unsigned int height)
