811b09ed25f825b3fa20177c15c84c58084698e8
TheArtist <agorglouk@gmail.com>
2018-05-04 16:05:43 +0300

    Added rand module

---
+++ src/rand.h
@@ -0,0 +1,63 @@
+/*********************************************************************************************************************/
+/*                                                  /===-_---~~~~~~~~~------____                                     */
+/*                                                 |===-~___                _,-'                                     */
+/*                  -==\\                         `//~\\   ~~~~`---.___.-~~                                          */
+/*              ______-==|                         | |  \\           _-~`                                            */
+/*        __--~~~  ,-/-==\\                        | |   `\        ,'                                                */
+/*     _-~       /'    |  \\                      / /      \      /                                                  */
+/*   .'        /       |   \\                   /' /        \   /'                                                   */
+/*  /  ____  /         |    \`\.__/-~~ ~ \ _ _/'  /          \/'                                                     */
+/* /-'~    ~~~~~---__  |     ~-/~         ( )   /'        _--~`                                                      */
+/*                   \_|      /        _)   ;  ),   __--~~                                                           */
+/*                     '~~--_/      _-~/-  / \   '-~ \                                                               */
+/*                    {\__--_/}    / \\_>- )<__\      \                                                              */
+/*                    /'   (_/  _-~  | |__>--<__|      |                                                             */
+/*                   |0  0 _/) )-~     | |__>--<__|     |                                                            */
+/*                   / /~ ,_/       / /__>---<__/      |                                                             */
+/*                  o o _//        /-~_>---<__-~      /                                                              */
+/*                  (^(~          /~_>---<__-      _-~                                                               */
+/*                 ,/|           /__>--<__/     _-~                                                                  */
+/*              ,//('(          |__>--<__|     /                  .----_                                             */
+/*             ( ( '))          |__>--<__|    |                 /' _---_~\                                           */
+/*          `-)) )) (           |__>--<__|    |               /'  /     ~\`\                                         */
+/*         ,/,'//( (             \__>--<__\    \            /'  //        ||                                         */
+/*       ,( ( ((, ))              ~-__>--<_~-_  ~--____---~' _/'/        /'                                          */
+/*     `~/  )` ) ,/|                 ~-_~>--<_/-__       __-~ _/                                                     */
+/*   ._-~//( )/ )) `                    ~~-'_/_/ /~~~~~~~__--~                                                       */
+/*    ;'( ')/ ,)(                              ~~~~~~~~~~                                                            */
+/*   ' ') '( (/                                                                                                      */
+/*     '   '  `                                                                                                      */
+/*********************************************************************************************************************/
+#ifndef _RAND_H_
+#define _RAND_H_
+
+/*
+ * MT19937 Generator
+ */
+
+/* Initializes mt[N] with a seed */
+void init_genrand(unsigned long s);
+
+/* Initialize by an array with array-length
+ * init_key is the array for initializing keys, key_length is its length */
+void init_by_array(unsigned long init_key[], int key_length);
+
+/* Generates a random number on [0,0xffffffff] interval */
+unsigned long genrand_int32(void);
+
+/* Generates a random number on [0,0x7fffffff] interval */
+long genrand_int31(void);
+
+/* Generates a random number on [0,1] real-interval */
+double genrand_real1(void);
+
+/* Generates a random number on [0,1) real-interval */
+double genrand_real2(void);
+
+/* Generates a random number on (0,1) real-interval */
+double genrand_real3(void);
+
+/* Generates a random number on [0,1) with 53-bit resolution */
+double genrand_res53(void);
+
+#endif /* ! _RAND_H_ */
