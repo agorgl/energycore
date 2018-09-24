c4d344889dd51fb451d99fc957d0f5082b75804d
TheArtist <agorglouk@gmail.com>
2018-09-25 01:13:39 +0300

    Improved exposure multiplier calculation shader

--- res/shaders/fx/eyeadapt_expo.glsl
+++ res/shaders/fx/eyeadapt_expo.glsl
@@ -1,6 +1,8 @@
 #version 430 core
-layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 16, local_size_y = 4, local_size_z = 1) in;
 #include "eyeadapt_buf.glsl"
+
+shared uint group_pyramid[NUM_HISTOGRAM_BINS];
 
 // Time since last exposure update
 uniform float delta_time;
@@ -11,13 +13,6 @@ const vec2 active_range = vec2(0.45, 0.95); // [0,1]
 
 // Minimum/Maximum average luminance to consider for auto exposure (in EV).
 const vec2 avglum_range = vec2(-5, 1);
-
-// Exposure bias. Use this to control the global exposure of the scene.
-// Used when dynamic_key_value is false
-const float fixed_key_value = 0.25;
-
-// Set this to true to handle the key value automatically based on average luminance.
-const bool dynamic_key_value = true;
 
 // Set this to true if you want the auto exposure to be animated (on = progressive, off = fixed)
 const bool progressive_adaptation = true;
@@ -33,41 +28,32 @@ const float speed_down = 1.0;
 //const vec2 lum_range = vec2(-8, 4); // [-16,1], [1,16]
 
 // Position is in [0,1] range
-float luminance_from_histogram_position(float position)
+float luminance_from_histogram_position(float position, float scale, float offs)
 {
-    float multiply = 1.0 / (lum_range.y - lum_range.x);
-    float add = -lum_range.x * multiply;
-    return exp2((position - add) / multiply);
+    return exp2((position - offs) / scale);
 }
 
-float interpolate_exposure(float new_exposure, float old_exposure, float delta_time, float speed_up, float speed_down)
+float interpolate_exposure(float new_exposure, float old_exposure, float speed_down, float speed_up, float delta_time)
 {
     float delta = new_exposure - old_exposure;
-    float speed = delta > 0.0 ? speed_up : speed_down;
+    float speed = delta > 0.0 ? speed_down : speed_up;
     float exposure = old_exposure + delta * (1.0 - exp2(-delta_time * speed));
-    //float exposure = old_exposure + delta * (delta_time * speed);
     return exposure;
 }
 
-float exposure_value()
+float average_luminance(float low_percent, float high_percent, float min_brightness, float max_brightness, float max_histogram_value, float scale, float offs)
 {
-    // Find maximum bin value
-    float max_value = 0.0;
-    for (uint i = 0; i < NUM_HISTOGRAM_BINS; ++i)
-        max_value = max(max_value, histogram[i]);
-    max_value = 1.0 / max_value;
-
     // Sum all bins
     float sum = 0;
     for (uint i = 0; i < NUM_HISTOGRAM_BINS; ++i)
-        sum += histogram[i] * max_value;
+        sum += histogram[i] * max_histogram_value;
 
     // Skip darker and lighter parts of the histogram to stabilize the auto exposure
     float filtered_sum = 0.0;
-    float accumulator = 0.0;
-    vec2 fraction_sums = sum * active_range;
+    float accumulator  = 0.0;
+    vec2 fraction_sums = sum * vec2(low_percent, high_percent);
     for (uint i = 0; i < NUM_HISTOGRAM_BINS; ++i) {
-        float bin_value = histogram[i] * max_value;
+        float bin_value = histogram[i] * max_histogram_value;
         // Filter dark areas
         float offs = min(fraction_sums.x, bin_value);
         bin_value -= offs;
@@ -76,25 +62,61 @@ float exposure_value()
         bin_value = min(fraction_sums.y, bin_value);
         fraction_sums.y -= bin_value;
         // Luminance at bin
-        float luminance = luminance_from_histogram_position(float(i) / NUM_HISTOGRAM_BINS);
+        float luminance = luminance_from_histogram_position(float(i) / NUM_HISTOGRAM_BINS, scale, offs);
         filtered_sum += luminance * bin_value;
         accumulator  += bin_value;
     }
-    // Average luminance
-    float avg_lum = clamp(filtered_sum / max(accumulator, 1.0e-4), avglum_range.x, avglum_range.y);
 
-    // Exposure value
-    avg_lum = max(1.0e-4, avg_lum);
-    float key_value = dynamic_key_value ? (1.03 - (2.0 / (2.0 + log2(avg_lum + 1.0)))) : fixed_key_value;
-    float exposure = key_value / avg_lum;
+    // Clamp to user brightness range
+    float avg_lum = clamp(filtered_sum / max(accumulator, 1.0e-4), min_brightness, max_brightness);
+    return avg_lum;
+}
+
+float log10(float n)
+{
+    const float kLogBase10 = 1.0 / log2(10.0);
+    return log2(n) * kLogBase10;
+}
+
+float exposure_multiplier(float avg_lum)
+{
+    float alum = max(1.0e-4, avg_lum);
+    float key_value = 0.18;
+    //float key_value = 1.03 - (2.0 / (2.0 + log10(alum + 1.0)));
+    float exposure = key_value / alum;
     return exposure;
 }
 
 void main()
 {
-    float exposure_prev = exposure_val;
-    float exposure = exposure_value();
-    exposure_val = progressive_adaptation
-        ? interpolate_exposure(exposure, exposure_prev, delta_time, speed_up, speed_down)
-        : exposure;
+    const uint thread_id = gl_LocalInvocationIndex;
+    group_pyramid[thread_id] = histogram[thread_id];
+
+    memoryBarrierShared();
+    barrier();
+
+    // Parallel reduction to find the max value
+    for (uint i = NUM_HISTOGRAM_BINS >> 1; i > 0; i >>= 1) {
+        if (thread_id < i)
+            group_pyramid[thread_id] = max(group_pyramid[thread_id], group_pyramid[thread_id + i]);
+        memoryBarrierShared();
+        barrier();
+    }
+
+    memoryBarrierShared();
+    barrier();
+
+    if (thread_id == 0) {
+        float exposure_prev = exposure_val;
+        float max_value = 1.0 / float(group_pyramid[0]);
+        float scale     = 1.0 / (lum_range.y - lum_range.x);
+        float offs      = -lum_range.x * scale;
+        float avg_lum   = average_luminance(active_range.x, active_range.y,
+                                            avglum_range.x, avglum_range.y,
+                                            max_value, scale, offs);
+        float exposure = exposure_multiplier(avg_lum);
+        exposure_val = progressive_adaptation
+            ? interpolate_exposure(exposure, exposure_prev, speed_down, speed_up, delta_time)
+            : exposure;
+    }
 }
